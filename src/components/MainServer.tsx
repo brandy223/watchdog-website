100e7ad9c44118c5c6c54f738c8a200140fc99cf
brandy223 <brandan.legall@gmail.com>
2023-08-02 22:47:51 +0200

    feat: socket test

--- src/components/MainServer.tsx
+++ src/components/MainServer.tsx
@@ -1,12 +1,22 @@
-import React from "react";
 
-// TODO: Replace by values in DB
+'use client'
 
-export const MainServer = ({ className }) => {
+import React, {useState, useEffect} from "react";
+import { useSocketConnection, useSocketEvent } from "@/api/socket";
+
+export const MainServer = () => {
+    const [mainServer, setMainServer] = useState<string>("192.168.10.54");
+
+    const socket = useSocketConnection("http://localhost:3001");
+
+    useSocketEvent(socket, "mainServer", (data: string) => {
+        setMainServer(data);
+    });
+
     return (
         <div>
             <a className="text-mainWhite font-medium rounded-full border-2 border-validGreen text-sm px-4 py-2 text-center mr-3 md:mr-0">
-                192.168.10.58
+                {mainServer}
             </a>
             <a className="text-mainWhite ml-4 font-medium rounded-full border-2 border-mainRed text-sm px-4 py-2 text-center mr-3 md:mr-0">
                 192.168.10.44
