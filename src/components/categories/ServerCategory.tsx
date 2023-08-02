d92d4bc4ca4e98e71a78dfd7836744e0b9edd237
brandy <brandan.le-gall@isen.yncrea.fr>
2023-08-02 17:02:44 +0200

    feat: servers services panel

---
+++ src/components/categories/ServerCategory.tsx
@@ -0,0 +1,36 @@
+import ServerServiceCell from "@/components/cells/ServerServiceCell";
+import {PrismaClient, Services, ServicesOfServers} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function getServicesOfServerById(id: number): Promise<Services[]> {
+    const services: ServicesOfServers[] = await prisma.servicesOfServers.findMany({
+        where: {
+            serverId: id
+        }
+    });
+    return prisma.services.findMany({
+        where: {
+            id: {in: services.map(service => service.serviceId)}
+        }
+    })
+}
+
+type ServerCategoryProps = {
+    id: number
+    ip: string
+}
+
+export default async function ServerCategory({id, ip}: ServerCategoryProps) {
+    const services: Services[] = await getServicesOfServerById(id);
+    return (
+        <div className="category-main-field-item w-full flex flex-col justify-center items-center">
+            <div className="category-main-field-item-title">{ip}</div>
+            <div className="category-main-field flex flex-col">
+                {services.map(
+                    (service: Services) => <ServerServiceCell key={service.id} id={service.id} name={service.name} />
+                )}
+            </div>
+        </div>
+    )
+}
